295c9a22f31086a541632fcd66cddb52272a8faa
zhuokim <138710184+zhuokim@users.noreply.github.com>
2025-03-16 21:34:46 +0800

    Fix deployment issues

--- api/database.py
+++ api/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# Use SQLite for Vercel
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# Use in-memory SQLite for Vercel
+SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
